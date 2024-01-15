24c5850f5c7c87bf0eac388c4f9b0b5e8b7790ca
Abirul Islam Abir <abir80503@gmail.com>
2024-01-15 17:09:18 +0600

    filter ui screen added

---
+++ lib/app/modules/view screen/filter screen/controllers/filter_screen_controller.dart
@@ -0,0 +1,51 @@
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:intl/intl.dart';
+
+import '../../../../model/category_model.dart';
+
+class FilterScreenController extends GetxController {
+  List<String> formattedDates = [];
+  bool isSelected = false;
+  RxInt selectedIndex = 0.obs;
+  String category = '';
+  List<DateTime?> selectedDates = [
+    DateTime.now(),
+  ];
+  List<User> userList = [
+    User(name: "All category"),
+    User(name: "Pathology "),
+    User(name: "Ultrasonography "),
+    User(name: "X-Ray "),
+    User(name: "ECG "),
+    User(name: "ECHO "),
+    User(name: "FNAC "),
+    User(name: "END/COM  "),
+    User(name: "EEG "),
+    User(name: "HISTO  "),
+  ];
+  final List<User>? selectedUserList = [];
+  int? selectedUserIndex;
+  onDateChange(dates) {
+    selectedDates = List<DateTime?>.from(dates);
+    formattedDates = selectedDates
+        .map((date) => DateFormat('yyyy-MM-dd').format(date!))
+        .toList();
+    print(formattedDates);
+  }
+  selectedChangeIndex (index) {
+    selectedUserIndex =
+    isSelected ? null : index;
+    update();
+  }
+  selectDateFilter(){
+       selectedIndex.value = 0;
+
+  }
+  selectCategoryFilter(){
+    selectedIndex.value = 1;
+  }
+  setCategory(){
+     print(category);
+  }
+}
