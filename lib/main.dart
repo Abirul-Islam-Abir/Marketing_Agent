c51bdaf0be224b0bdb07dbab8c479a3a8bbba440
abir8 <abir80503@gmail.com>
2024-02-01 02:44:54 +0600

    push notification implement in this project

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'app/app info/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'app/data/const/export.dart';
+import 'app/modules/Fcm Notification/controller/fcm_notification_controller.dart';
+import 'app/modules/Fcm Notification/local notification/local_notification.dart';
 import 'firebase_options.dart';
+
 Future<void> main() async {
+  await initializeApp();
+}
+
+Future initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await GetStorage.init();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  NotificationService().initializeApp();
+  Get.put(FcmMessagingController());
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
@@ -18,4 +28,12 @@ Future<void> main() async {
     ),
     //const AminAgent(),
   );
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  print('Handling a background message ${message.messageId}');
 }
