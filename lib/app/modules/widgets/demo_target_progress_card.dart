608afaa9fa8fb942d5d630ae63390dad5419622b
abir8 <abir80503@gmail.com>
2024-02-03 10:03:52 +0600

    completed targets

---
+++ lib/app/modules/widgets/demo_target_progress_card.dart
@@ -0,0 +1,20 @@
+import '../../data/const/export.dart';
+
+class DemoTargetProgressCard extends StatelessWidget {
+  const DemoTargetProgressCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AgentsTargetedProgressCard(
+      isCurrent: false,
+      onTap: () async {},
+      text: 'There is no target',
+      progress: '0.0',
+      agentsCount: 0,
+      amountCollected: '0.0',
+      targetAmount: '0.0',
+    );
+  }
+}
---
+++ lib/app/modules/widgets/empty_list_text.dart
@@ -0,0 +1,19 @@
+import '../../data/const/export.dart';
+
+class EmptyListText extends StatelessWidget {
+  const EmptyListText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Text(
+          'Empty List',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: AppColor.kWhiteColor),
+        ));
+  }
+}
